64317b6f5c894ab69e3afe644e8fd60a21f05017
soberchan <sobersguild@gmail.com>
2024-05-23 21:25:35 -0300

    alters map and entities generation

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1715659301|663278394";
+const CACHE_VERSION = "1716510115|14450570914";
 const CACHE_PREFIX = "Breadfinder Band-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
